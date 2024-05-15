4474175b032e5c379edceea61648aa3b2b1ba54d
Veronica-Alfr <veagalves@gmail.com>
2024-05-15 17:42:43 -0300

    feat: add theme

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+
+
+def index(request):
+    return render(request, 'products.html')
+
+def products_id(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    # Passa o produto como contexto para o template
+    context = {}
+    context['product'] = product
+    # ou {'product': product} passado no 3° parâmetro abaixo:
+    return render(request, 'productsId.html', context)
